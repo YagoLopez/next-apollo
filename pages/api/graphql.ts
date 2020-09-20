a98183a8b3b53a494a968e6a979eedab9b9078fe
Yago Lopez <yago.lopez@gmail.com>
2020-09-20 15:02:28 +0200

    Enable ApolloServer playground on production

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -32,7 +32,11 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+});
 
 const handler = server.createHandler({ path: "/api/graphql" });
 
