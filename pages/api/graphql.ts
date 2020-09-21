2a1e630d2e9f2b645253cd71cf5ecf960e2fe668
Yago Lopez <yago.lopez@gmail.com>
2020-09-21 14:57:58 +0200

    Use apollo server context for data
    And add raw node api endpoint

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,10 +2,10 @@ import { ApolloServer, gql } from "apollo-server-micro";
 import _remove from "lodash/remove"
 
 let recipes = [
-    { id: 1, title: "Recipe 1", averageRating: 10 },
-    { id: 2, title: "Recipe 2", averageRating: 20 },
-    { id: 3, title: "Recipe 3", averageRating: 30 }
-  ]
+  { id: 1, title: "Recipe 1", averageRating: 10 },
+  { id: 2, title: "Recipe 2", averageRating: 20 },
+  { id: 3, title: "Recipe 3", averageRating: 30 }
+]
 
 const typeDefs = gql`
   type Recipe {
@@ -21,16 +21,17 @@ const typeDefs = gql`
   type Mutation {
     removeRecipe(id: ID): Recipe
   }
-
 `;
 
 const resolvers = {
   Query: {
-    recipes: () => recipes,
+    recipes: (_, __, context) => context.recipes,
   },
 
   Mutation: {
-    removeRecipe: (_, { id }) => _remove(recipes, (recipe) => recipe.id === +id)[0]
+    removeRecipe: (_, { id }, context) => {
+      return _remove(context.recipes, (recipe) => recipe.id === +id)[0]
+    }
   },
 };
 
@@ -38,7 +39,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
-  introspection: true
+  introspection: true,
+  context: { recipes }
 });
 
 const handler = server.createHandler({ path: "/api/graphql" });
