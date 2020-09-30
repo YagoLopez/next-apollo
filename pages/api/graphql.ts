0e45de071a93a47e1e2462ea3b7eface820a9260
Yago Lopez <yago.lopez@gmail.com>
2020-09-30 15:10:19 +0200

    test 52

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,52 +1,9 @@
-const cors = require('micro-cors')(); // highlight-line
 import { ApolloServer, gql } from "apollo-server-micro";
-
-// const { PHASE_PRODUCTION_BUILD } = require('next/constants')
-// var db = new sqlite3.Database( path.resolve(__dirname, 'db.sqlite') );
-const path = require('path')
-const fullDbpath = path.resolve('data.db')
-
-let filename
-console.log('dirname', __dirname)
-console.log('filename', __filename)
-console.log('fullDbPath', fullDbpath)
-console.log('process.cwd()', process.cwd())
-console.log('resolve', path.resolve('.'))
-
-
-declare const process
-
-if (process.env.NODE_ENV === 'production') {
-  filename = './_next/static/data.db'
-}
-
-if (process.env.NODE_ENV === 'development') {
-  filename = './data.db'
-}
-
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-
-
-const fullDbPath2 = path.join(process.cwd(), path.sep + 'data.db')
-console.log('fullDbPath2', fullDbPath2)
-
-// const db = require("knex")({
-//   client: "pg",
-//   connection: {
-//     host: "ec2-54-247-79-178.eu-west-1.compute.amazonaws.com",
-//     user: "vniyjodlqouigi",
-//     password: "ea694508a7a30456653341727a9bc3ee7aa1e55bc89a0c808d49d283e43a9554",
-//     database: "d4c5jva0v72hpk",
-//     ssl: { rejectUnauthorized: false }
-//   }
-// });
-
-const db = null
 
 const typeDefs = gql`
   type Item {
-    id: Int!
-    text: String!
+    id: Int
+    text: String
   }
   
   type Query {
@@ -61,34 +18,16 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    items: async (_, __, { db }) => {
-      // const result = await db.select('*').from('items')
-      // return [
-      //   {
-      //     "id": 1,
-      //     "text": "Item",
-      //     "__typename": "Item"
-      //   },
-      //   {
-      //     "id": 3,
-      //     "text": "Item",
-      //     "__typename": "Item"
-      //   }
-      // ]
+    items: async (_, __) => {
       return []
-
     }
   },
 
   Mutation: {
-    removeItem: async (_, { id }, { db }) => {
-      // const removed_rows = await db('items').where({ id }).del()
-      // return id
+    removeItem: async (_, { id }) => {
       return 3
     },
-    addItem: async (_, { text }, { db }) => {
-      // const idList: number[] = await db('items').insert({text: 'Item'}).returning('id')
-      // return { id: idList[0], text }
+    addItem: async (_, { text }) => {
       return { id: 4, text: "Item" }
     }
   }
@@ -99,7 +38,7 @@ const server = new ApolloServer({
   resolvers,
   playground: true,
   introspection: true,
-  context: (req, res) => ({ req, res, db })
+  context: (req, res) => ({ req, res })
 })
 
 const handler = server.createHandler({ path: '/api/graphql' });
@@ -110,6 +49,4 @@ export const config = {
   },
 };
 
-// export default cors(handler);
 export default handler;
-// module.exports = cors((req, res) => req.method === 'OPTIONS' ? res.end() : handler(req, res))
